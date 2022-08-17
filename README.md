eec9d9ef7993926abef7a3c33a833e7dc8d649b7
gavinschultz <minif.development@gmail.com>
2022-08-16 23:09:29 -0700

    Finished work

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # gpx-to-csv
 A simple tool that converts and compiles .gpx files into .csv files.
+
+###Requires
+- Python 3.6
+
+###Usage
+- Download repository to computer
+- Place .gpx files into the /input folder
+- In terminal, run the following command: 
+  - On mac, use python3 rather than python
+```
+cd /path/to/gpx-to-csv
+python main.py
+```
+- Points should now be found in .csv
+
+###License
+This software is licenced under the [public domain](https://github.com/minif/gpx-to-csv/blob/main/LICENSE). 
+You may use it in any way without restriction. 
+
+[utm](https://github.com/Turbo87/utm), used for coordinate conversion, is licenced under
+[MIT]((https://github.com/Turbo87/utm/blob/81987caa30465e963a9163b6d701ea568f67af9f/LICENSE)).
--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from xml.dom.minidom import parse
+from os import listdir
 import utm.conversion
-doc = parse('/Users/minif/Desktop/test.GPX')#DEBUG/WIP!
+
+INPUT_DIR = "input/"
+OUTPUT_CSV = "output.csv"
 
 def get_point_name(point):
+    """
+    Gets name of a point as a string.
+    :param point: Proper DOM object waypoint (wpt) from a .gpx (1.1) file
+    :return: String, with name
+    """
     name = point.getElementsByTagName("name")[0].firstChild.nodeValue
     return name
 
 def get_point_UTM(point):
+    """
+    Gets UTM coordinates from point as a set. Converts from lat/lon to UTM.
+    :param point: Proper DOM object waypoint (wpt) from a .gpx (1.1) file
+    :return: UTM coordinates
+    """
     lat = float(point.getAttribute("lat"))
     lon = float(point.getAttribute("lon"))
     UTM = utm.conversion.from_latlon(lat, lon)
     return UTM
 
 def get_point_ele(point):
+    """
+    Gets elevation of a point as a string.
+    :param point: Proper DOM object waypoint (wpt) from a .gpx (1.1) file
+    :return: String, with elevation
+    """
     name = point.getElementsByTagName("ele")[0].firstChild.nodeValue
     return name
 
 def returnPointInfo(point):
     """
-    Gets details of a point as a string. This is useful for debugging.
+    Gets details of a point as a string. Only includes easting and northing.
     :param point: Proper DOM object waypoint (wpt) from a .gpx (1.1) file
     :return: String, with name, lat, long, and elevation (comma seperated)
     """
-    text = "%s,%s,%s"%(get_point_name(point),get_point_UTM(point),get_point_ele(point))
+    easting, northing, e, e = get_point_UTM(point)
+    text = "%s,%s,%s,%s"%(get_point_name(point), round(easting), round(northing),round(float(get_point_ele(point))))
     return text
 
 def main():
@@ -30,7 +49,18 @@ def main():
     Main program function. Finds and processes all waypoints and parses them.
     :return: none
     """
-    points = doc.getElementsByTagName("wpt")
-    for i in points:
-        print(returnPointInfo(i))
+    f = open(OUTPUT_CSV, "w")
+    f.write("name,easting,northing,elevation\n")
+    for gpx in listdir(INPUT_DIR):
+        print("Reading %s..."%INPUT_DIR+gpx)
+        try:
+            doc = parse(INPUT_DIR+gpx)
+            points = doc.getElementsByTagName("wpt")
+            for i in points:
+                print(returnPointInfo(i))
+                f.write(returnPointInfo(i)+"\n")
+        except:
+            print("Error reading %s!"%INPUT_DIR+gpx)
+
+    f.close()
 main()
